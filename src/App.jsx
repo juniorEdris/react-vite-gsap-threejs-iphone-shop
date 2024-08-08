ce001e456f496bdc9c25410229a74c2753f4805f
Eftekar Raghib <70286969+juniorEdris@users.noreply.github.com>
2024-08-08 17:40:08 +0600

    updated 3d model on the websites

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
-import Model from "./components/Model";
+// import Model from "./components/Model";
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
 import Footer from "./components/Footer";
+import Model2 from "./components/Model2";
 console.log("")
 // import * as Sentry from '@sentry/react';
 
@@ -14,7 +15,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Highlights />
-      <Model />
+      <Model2 />
       <Features />
       <HowItWorks />
       <Footer />
